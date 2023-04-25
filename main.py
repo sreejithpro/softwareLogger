2d76ad9e3031fadd854599fe2762ca93d1d44b7a
sreejithpro <sreejithpro@gmail.com>
2023-04-25 11:42:11 +0100

    multiple process

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+import os
 import psutil
 import win32api
 import tkinter as tk
-import os
 
-process_name = "notepad.exe"  # Change this to the process name you want to monitor
-log_file = r'P:\INBLC\W&E\GE\Geotech\Shared Documents\E. Softwares\Software Tracker\process_log.txt'  # Change this to the path of the log file on the network drive
+process_names = ["notepad.exe", "calculator.exe", "explorer.exe"]  # Change this to the process names you want to monitor
+log_file = "process_log.txt"  # Change this to the path of the log file on the network drive
+max_lines = 50  # Change this to the maximum number of lines to keep in the log file
 
 
-def check_process():
+def check_processes():
+    running_processes = []
     for proc in psutil.process_iter(['pid', 'name']):
-        if proc.name() == process_name:
+        if proc.name() in process_names:
+            running_processes.append(proc.name())
             username = win32api.GetUserName()
-            message = f"User {username} has started using {process_name}."
-            with open(log_file, "a") as f:
+            message = f"User {username} has started using {proc.name()}."
+            with open(log_file, "r+") as f:
+                lines = f.readlines()
+                f.seek(0)
                 f.write(message + "\n")
-            label.config(text=message)
-            break
-    else:
-        message = f"No process with the name {process_name} is running."
+                f.writelines(lines)
+                f.truncate(max_lines * (len(lines) // max_lines + 1))
+    if running_processes:
+        message = f"User {username} is currently using the following processes: {', '.join(running_processes)}."
         label.config(text=message)
-    root.after(1000, check_process)
+    else:
+        message = f"No processes are running."
+        label.config(text=message)
+    root.after(1000, check_processes)
 
 
 def read_log_file():
@@ -36,12 +44,12 @@ def read_log_file():
 root = tk.Tk()
 root.title("Process Tracker")
 
-label = tk.Label(root, text="Checking process...")
+label = tk.Label(root, text="Checking processes...")
 label.pack(padx=10, pady=10)
 
 log_label = tk.Label(root, text=read_log_file())
 log_label.pack(padx=10, pady=10)
 
-root.after(1000, check_process)
+root.after(1000, check_processes)
 
 root.mainloop()
