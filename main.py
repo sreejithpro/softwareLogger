f2a7e9303a7acf62fdf8f5a2d8e2a629c9fd0210
sreejithpro <sreejithpro@gmail.com>
2023-04-25 12:05:21 +0100

    multiple process

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import os
 import psutil
 import win32api
 import tkinter as tk
+from tkinter import ttk
 
-process_names = ["notepad.exe", "calculator.exe", "explorer.exe"]  # Change this to the process names you want to monitor
+process_names = ["notepad.exe", "calc.exe", "explorer.exe"]  # Change this to the process names you want to monitor
 log_file = "process_log.txt"  # Change this to the path of the log file on the network drive
-max_lines = 50  # Change this to the maximum number of lines to keep in the log file
+max_lines = 100  # Change this to the maximum number of lines to keep in the log file
 
 
 def check_processes():
-    running_processes = []
-    for proc in psutil.process_iter(['pid', 'name']):
+    running_processes = {}
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
         if proc.name() in process_names:
-            running_processes.append(proc.name())
+            running_processes[proc.name()] = running_processes.get(proc.name(), set())
+            running_processes[proc.name()].add(proc.username())
             username = win32api.GetUserName()
             message = f"User {username} has started using {proc.name()}."
             with open(log_file, "r+") as f:
@@ -21,12 +23,14 @@ def check_processes():
                 f.write(message + "\n")
                 f.writelines(lines)
                 f.truncate(max_lines * (len(lines) // max_lines + 1))
-    if running_processes:
-        message = f"User {username} is currently using the following processes: {', '.join(running_processes)}."
-        label.config(text=message)
-    else:
-        message = f"No processes are running."
-        label.config(text=message)
+    for process_name in process_names:
+        users = sorted(running_processes.get(process_name, set()))
+        if not tree.exists(process_name):
+            tree.insert("", "end", process_name, text=process_name)
+        for i, user in enumerate(users):
+            tree.set(process_name, i + 1, user)
+        for j in range(len(users), tree.column("#") - 1):
+            tree.set(process_name, j + 1, "")
     root.after(1000, check_processes)
 
 
@@ -44,8 +48,14 @@ def read_log_file():
 root = tk.Tk()
 root.title("Process Tracker")
 
-label = tk.Label(root, text="Checking processes...")
-label.pack(padx=10, pady=10)
+tree = ttk.Treeview(root, columns=["#" + str(i) for i in range(len(process_names) + 1)], show="headings")
+tree.heading("#0", text="")
+for i, process_name in enumerate(process_names):
+    tree.heading("#" + str(i + 1), text=process_name)
+tree.column("#0", width=100, anchor="w")
+for i in range(len(process_names)):
+    tree.column("#" + str(i + 1), width=200, anchor="w")
+tree.pack(padx=10, pady=10)
 
 log_label = tk.Label(root, text=read_log_file())
 log_label.pack(padx=10, pady=10)
